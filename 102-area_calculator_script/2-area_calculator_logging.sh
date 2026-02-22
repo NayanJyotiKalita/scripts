100260509488e635d8236e606bc803d87533d599
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2026-02-23 03:17:43 +0530

    Update and rename 2-area_calculator_logging.sh to 2-area_calculator_logging.sh

--- 102-area_calculator_script/2-area_calculator_logging.sh
+++ 102-area_calculator_script/2-area_calculator_logging.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #####################################################################
-# Author: Dhama Dhora
+# Author: Nayan Jyoti Kalita
 # Version: v0.0.2
 #
 # Script to calculate the area of geometric shapes with logging.
